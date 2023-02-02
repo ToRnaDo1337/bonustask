30248e74e654d9965221ef42329390be18b95f36
Eldar <eldar.sharapiev@gmail.com>
2023-02-02 18:41:58 +0600

    any changes

--- test/printing.go
+++ test/printing.go
@@ -6,10 +6,14 @@ import (
 	"github.com/ToRnaDo1337/bonustask/math"
 )
 
+// PrintCalculatorResult prints the result of a calculation based on the operation
 func PrintCalculatorResult(c *math.Calculator, operation string) {
 	var result int
 	switch operation {
-
+	case "add":
+		result = math.Add(c.Num1, c.Num2)
+	case "subtract":
+		result = math.Subtract(c.Num1, c.Num2)
 	case "multiply":
 		result = c.Multiply()
 	case "divide":
